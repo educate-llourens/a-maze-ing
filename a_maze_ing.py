22d425f8afceb809124e352633abb0fb1bc5b71f
llourens <llourens@f0r3s7.codam.nl>
2026-03-17 17:44:43 +0100

    🧪 Added tests for config file

---
+++ a_maze_ing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def parse_config_file() -> dict:
+    config_dict: dict = {}
+    return config_dict
+
+
+def main() -> None:
+    parse_config_file()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_config_file.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from a_maze_ing import parse_config_file
+
+
+def test_retrieve_width():
+    config_info: dict = parse_config_file()
+
+    assert config_info["WIDTH"] == 20
+
+
+def test_retrieve_all_items():
+    config_info: dict = parse_config_file()
+
+    assert config_info["HEIGHT"] == 25
+    assert config_info["ENTRY"] == [0, 0]
+    assert config_info["EXIT"] == [19, 14]
+    assert config_info["OUTPUT_FILE"] == "maze.txt"
+    assert config_info["PERFECT"] is True
+
+
+def test
+
+
+def main() -> None:
+    test_retrieve_width()
+    test_retrieve_all_items()
+
+
+if __name__ == "__main__":
+    main()
