4bfa3f93e0afdcabefaee1430615e44a51fdf4e5
Leandra Lourens <llourens.codam@gmail.com>
2026-03-20 13:25:46 +0100

    🚧 Made some changes to the tests and the main file

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
-def parse_config_file(config_file: str) -> dict:
-    pass
+# Error Handling --------------------------------------------------------------
 
+class ConfigError(Exception):
+    def __int__(self, msg: str):
+        self.msg = msg
+
+
+class InputError(Exception):
+    def __int__(self, msg: str):
+        self.msg = msg
+
+# Input checking --------------------------------------------------------------
 
 def input_checks() -> None:
     pass
 
+# Parsing ---------------------------------------------------------------------
+
+def parse_config_file(config_file: str) -> dict:
+    pass
 
 def main() -> None:
-    pass
+    input_checks
 
 
 if __name__ == "__main__":
