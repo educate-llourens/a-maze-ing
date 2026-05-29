ce157bbcd9f78780cd110a38e92602795b019269
LionessLilo <leandra@lionesslilo.com>
2026-05-29 16:13:06 +0200

    🚧 Made basically no progress

--- visuals/display_maze.py
+++ visuals/display_maze.py
@@ -7,6 +7,7 @@ from typing import Any
 # from enum import Enum
 from mlx import Mlx
 import signal
+import os
 
 # W S E N
 
@@ -38,26 +39,62 @@ import signal
 #     EW = 5
 
 
+class MlxData:
+    def __init__(self):
+        self.mlx = Mlx()
+        self.mlx_ptr = self.mlx.mlx_init()
+        if self.mlx_ptr is None:
+            raise DisplayError("Unable to initialise mlx")
+
+class WindowData:
+    def __init__(self, configs: dict, mlx: MlxData):
+        try:
+            self.width = configs["WIDTH"] * 32
+            self.height = configs["HEIGHT"] * 32
+        except KeyError:
+            raise ConfigError("No 'WIDTH or 'HEIGHT' key in "
+                                "config for mlx")
+        self.window_ptr = mlx.mlx.mlx_new_window(mlx.mlx_ptr, self.width,
+                                    self.height, "a-maze-ing")
+
+class ImageData:
+    def __init__(self, mlx: MlxData, window: WindowData):
+        self.image_ptr = mlx.mlx.mlx_new_image(mlx.mlx_ptr, window.width,
+                                               window.height)
+
+
+def create_image(image: ImageData, mlx: MlxData, window: WindowData) -> None:
+    y = 100
+    while y < 300:
+        x = 100
+        while x < 400:
+            mlx.mlx.mlx_pixel_put(mlx.mlx_ptr, window.window_ptr,
+                                  x, y, 0xFFFFFF)
+            x += 1
+        y += 1
+
+
 def mlx_display(maze: list[list[int]], configs: dict) -> None:
-    mlx: Mlx = Mlx()
-    window_ptr: Any
-    window_width: int
-    window_height: int
-    window_ptr: Any
+    mlx: MlxData = MlxData()
+    window = WindowData(configs, mlx)
+#     # image = ImageData(mlx, window)
+
+#     # create_image(image, mlx, window)
+#     def draw(param):
+#         y = 100
+#         while y < 300:
+#             x = 100
+#             while x < 400:
+#                 mlx.mlx.mlx_pixel_put(mlx.mlx_ptr, window.window_ptr, x, y, 0xFFFFFF)
+#                 x += 1
+#             y += 1
 
     try:
-        window_width = configs["WIDTH"] * 2
-        window_height = configs["HEIGHT"] * 2
-    except KeyError:
-        raise ConfigError("No 'WIDTH or 'HEIGHT' key in config for mlx")
-    initialised_mlx = mlx.mlx_init()
-    if initialised_mlx is None:
-        raise DisplayError("Unable to initialise mlx")
-    window_ptr = mlx.mlx_new_window(initialised_mlx, window_width,
-                                    window_height, "a-maze-ing")
-    signal.signal(signal.SIGINT, mlx.mlx_loop_exit(initialised_mlx))
-    mlx.mlx_loop(initialised_mlx)
-    mlx.mlx_destroy_window(initialised_mlx, window_ptr)
+        mlx.mlx.mlx_loop(mlx.mlx_ptr)
+    except KeyboardInterrupt:
+        print("Error: Keyboard input interrupted the program. Shuttingdown")
+        signal.signal(signal.SIGINT, os._exit(0))
+#     mlx.mlx.mlx_destroy_window(mlx.mlx_ptr, window.window_ptr)
 
 
 def display_maze(configs: dict) -> None:
